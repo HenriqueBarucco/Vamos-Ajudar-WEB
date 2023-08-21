2dc952400e9a173a8f074c8c6dc955cfd962e95f
Henrique Barucco <henrique_barucco@hotmail.com>
2023-08-21 16:56:17 -0300

    feat(page.tsx): adiciona imagens de logo e download Adiciona as imagens de logo e download ao componente de página. As imagens são importadas do diretório público e exibidas usando o componente Image do Next.js. Isso melhora a aparência e a experiência do usuário, adicionando elementos visuais relevantes à página.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image';
+
 export default function Home() {
     return (
-        <main className='flex justify-center text-center items-center h-screen '>
-            <p>Vamos Ajudar?</p>
+        <main className='flex flex-col justify-center text-center items-center h-screen bg-[#1a1f24]'>
+            <p className='text-3xl text-slate-100'>Vamos Ajudar?</p>
+            <Image src={'/logo.png'} alt='logo' width={300} height={300} />
+            <Image src={'/download.png'} alt='logo' width={150} height={150} />
         </main>
     );
 }
