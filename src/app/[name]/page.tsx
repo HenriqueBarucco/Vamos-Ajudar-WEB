5a92102b586d3c1be0a56cbb6aa498e1eb14bf3f
Henrique Barucco <henrique_barucco@hotmail.com>
2023-08-21 16:39:23 -0300

    feat(.editorconfig): adiciona arquivo .editorconfig com configurações de formatação de código feat(.eslintrc.json): adiciona configurações do ESLint para linting e formatação de código feat(.prettierrc): adiciona configurações do Prettier para formatação de código chore(package.json): atualiza a versão para 1.0.0 chore(public): remove os arquivos next.svg e vercel.svg feat(src/app/[name]/page.tsx): adiciona página para exibir informações de uma ONG específica chore(src/app/globals.css): remove configurações de cores do arquivo globals.css
    As alterações foram feitas para adicionar arquivos de configuração (.editorconfig, .eslintrc.json, .prettierrc) para melhorar a consistência e a formatação do código. A versão
    
    refactor(layout.tsx): atualiza metadados do layout
    refactor(page.tsx): simplifica o conteúdo da página inicial
    Os metadados do layout foram atualizados para refletir o título e a descrição corretos do aplicativo. Além disso, o conteúdo da página inicial foi simplificado para exibir apenas um texto de "Vamos Ajudar?" centralizado na tela. Essas alterações visam melhorar a experiência do usuário e fornecer uma interface mais limpa e direta.

---
+++ src/app/[name]/page.tsx
@@ -0,0 +1,40 @@
+export default async function Ong({ params }: { params: { name: string } }) {
+    const ong = await fetch(`https://vamos-ajudar-api.henriquebarucco.com.br/v1/ong/${params.name}`);
+
+    const jsonData = await ong.json();
+
+    return (
+        <div className="min-h-screen bg-[#1a1f24] flex justify-center items-center">
+            <div className="bg-white rounded-lg shadow-md p-8 w-96">
+                <h1 className="text-2xl font-bold mb-4">{jsonData.name}</h1>
+                <p className="text-gray-600 mb-2">{jsonData.description}</p>
+                <p className="text-gray-600 mb-4">Objetivo: {jsonData.goal}</p>
+    
+                <div className="border-t pt-4">
+                    <h2 className="text-lg font-semibold mb-2">Endereço</h2>
+                    <p className="text-gray-600">{jsonData.address.street}, {jsonData.address.number}</p>
+                    <p className="text-gray-600">{jsonData.address.city}, {jsonData.address.state}, {jsonData.address.postalCode}</p>
+                    <p className="text-gray-600">{jsonData.address.country}</p>
+                </div>
+    
+                <div className="border-t pt-4">
+                    <h2 className="text-lg font-semibold mb-2">Contato</h2>
+                    <p className="text-gray-600">Email: {jsonData.contact.email}</p>
+                    <p className="text-gray-600">Telefone: {jsonData.contact.phone}</p>
+                    <p className="text-gray-600">Site: {jsonData.contact.website}</p>
+                </div>
+    
+                <div className="border-t pt-4">
+                    <p className={`text-${jsonData.verified ? 'green' : 'red'}-600 font-semibold`}>
+                        {jsonData.verified ? 'Verificado' : 'Não verificado'}
+                    </p>
+                    <p className={`text-${jsonData.active ? 'green' : 'red'}-600 font-semibold`}>
+                        {jsonData.active ? 'Ativo' : 'Inativo'}
+                    </p>
+                </div>
+    
+                <p className="text-gray-600 mt-4">Criado em: {jsonData.createdAt}</p>
+            </div>
+        </div>
+    );
+}
