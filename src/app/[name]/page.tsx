91d07399ec34cb816be5208ef666ff73bf965d6c
Henrique Barucco <henrique_barucco@hotmail.com>
2023-09-11 11:37:03 -0300

    feat(next.config.js): adiciona configuração de imagens remotas feat(src/app/[name]/page.tsx): adiciona importação de ícone de estrela e componente de imagem do Next.js fix(src/app/[name]/page.tsx): adiciona exibição da imagem da ONG e ícone de estrela feat(src/components/icons/star.tsx): adiciona componente de ícone de estrela fix(tailwind.config.ts): corrige formatação do arquivo de configuração do Tailwind CSS
    As alterações foram feitas para adicionar suporte a imagens remotas no Next.js, exibir a imagem da ONG e adicionar um ícone de estrela para indicar se a ONG é verificada. Além disso, foi adicionado um botão para abrir a página da ONG no aplicativo. Também foi adicionado um novo componente de í

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -1,3 +1,6 @@
+import { StarIcon } from '@/components/icons/star';
+import Image from 'next/image';
+
 export default async function Ong({ params }: { params: { name: string } }) {
     const ong = await fetch(`https://vamos-ajudar-api.henriquebarucco.com.br/v1/ong/${params.name}`);
 
@@ -14,7 +17,16 @@ export default async function Ong({ params }: { params: { name: string } }) {
     return (
         <div className="min-h-screen bg-[#1a1f24] flex justify-center items-center">
             <div className="bg-white rounded-lg shadow-md p-8 w-96">
-                <h1 className="text-2xl font-bold mb-4">{jsonData.name}</h1>
+                <div className=''>
+                    <Image src={jsonData.image} alt='ong-img' width={550} height={200} />
+                </div>
+                <div className='flex flex-row justify-between pt-4'>
+                    <h1 className="text-2xl font-bold mb-4">{jsonData.name}</h1>
+
+                    <p className={`text-${jsonData.verified ? 'green' : 'red'}-600 font-semibold`}>
+                        {jsonData.verified ? <StarIcon/> : ''}
+                    </p>
+                </div>
                 <p className="text-gray-600 mb-2">{jsonData.description}</p>
                 <p className="text-gray-600 mb-4">Objetivo: {jsonData.goal}</p>
     
@@ -31,17 +43,12 @@ export default async function Ong({ params }: { params: { name: string } }) {
                     <p className="text-gray-600">Telefone: {jsonData.contact.phone}</p>
                     <p className="text-gray-600">Site: {jsonData.contact.website}</p>
                 </div>
-    
-                <div className="border-t pt-4">
-                    <p className={`text-${jsonData.verified ? 'green' : 'red'}-600 font-semibold`}>
-                        {jsonData.verified ? 'Verificado' : 'Não verificado'}
-                    </p>
-                    <p className={`text-${jsonData.active ? 'green' : 'red'}-600 font-semibold`}>
-                        {jsonData.active ? 'Ativo' : 'Inativo'}
-                    </p>
-                </div>
-    
                 <p className="text-gray-600 mt-4">Criado em: {jsonData.createdAt}</p>
+                <div className='flex justify-center pt-6'>
+                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                        Abrir no App
+                    </button>
+                </div>
             </div>
         </div>
     );
